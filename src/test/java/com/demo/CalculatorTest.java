d397e176fb15582f1e7cace1a0b5e8447946c683
sujithiran68 <sujithiran68@gmail.com>
2026-05-08 11:49:33 +0530

    Fix headless Jenkins test issue

--- src/test/java/com/demo/CalculatorTest.java
+++ src/test/java/com/demo/CalculatorTest.java
@@ -1,30 +1,29 @@
 package com.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    Calculator c = new Calculator(false);
+    CalculatorService calc = new CalculatorService();
 
     @Test
     void testAddPositiveNumbers() {
-        assertEquals(5, c.add(2, 3));
+        assertEquals(5, calc.add(2,3));
     }
 
     @Test
     void testAddNegativeNumbers() {
-        assertEquals(-5, c.add(-2, -3));
-    }
-
-    @Test
-    void testAddWithZero() {
-        assertEquals(7, c.add(7, 0));
+        assertEquals(-5, calc.add(-2,-3));
     }
 
     @Test
     void testAddMixedNumbers() {
-        assertEquals(1, c.add(3, -2));
+        assertEquals(1, calc.add(3,-2));
+    }
+
+    @Test
+    void testAddWithZero() {
+        assertEquals(5, calc.add(5,0));
     }
 }
